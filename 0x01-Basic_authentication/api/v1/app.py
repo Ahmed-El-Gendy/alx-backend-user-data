76a56a439ca04ef2e8f1049e8f61106d59086392
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-09-20 16:17:52 +0000

    task5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,7 +23,9 @@ def before_request():
     """Before request handler"""
     if auth is None:
         return
-    if auth.require_auth(request.path, ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]):
+    if auth.require_auth(
+        request.path, ["/api/v1/status/", "/api/v1/unauthorized/",
+                       "/api/v1/forbidden/"]):
         if not auth.authorization_header(request):
             abort(401)
         if not auth.current_user(request):
